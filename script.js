5182533c973740583a1adac74fe059ecdb23cb56
leah-git1 <l0583263572@gmail.com>
2025-11-03 23:49:17 +0200

    add text

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 alert("How are you??")
-alert("this is a big conflict!!!")
+alert("this is a big conflict!!!!")
 
-<<<<<<< HEAD
 alert("hey my name is leah")
-=======
-alert("hey")
->>>>>>> c360576564e146279b8f35755506bdf9aae20214
+
+
